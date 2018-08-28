28849789dfe027327447a297ddcbd9999a1ee088
Nick Spiel <nick.spiel@rea-group.com>
2018-08-29 09:04:36 +1000

    Added web component

--- main.mjs
+++ main.mjs
@@ -1,3 +1,15 @@
 const template = document.getElementById('my-paragraph');
 const templateContent = template.content;
 document.body.appendChild(templateContent);
+
+customElements.define('my-paragraph',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      let template = document.getElementById('my-paragraph');
+      let templateContent = template.content;
+
+      this.attachShadow({mode: 'open'})
+        .appendChild(templateContent.cloneNode(true));
+  }
+})
